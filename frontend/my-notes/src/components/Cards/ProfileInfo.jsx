445ac5e215aacc1664b3184b4ee9d828470e33a2
Sachindu Dilshan <pasachiya@gmail.com>
2024-07-03 00:57:36 +0530

    Finish creating frontend
    finish Home page
    add components related to home page

--- frontend/my-notes/src/components/Cards/ProfileInfo.jsx
+++ frontend/my-notes/src/components/Cards/ProfileInfo.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { getInitials } from '../../utils/helper'
 
-const ProfileInfo = (OnLogout) => {
+const ProfileInfo = (onLogout) => {
   return (
     <div className='flex items-center gap-3'>
         <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>
-        Jhon Willam
+        {getInitials("Jhon Willam")}
         </div>
 
         <div>
-            <p className='text-sm font-medium'>Willam</p>
-            <button className='text-sm text-slate-700 underline' onClick = {OnLogout}>
+            <p className='text-sm font-medium'>Jhon Willam</p>
+            <button className='text-sm text-slate-700 underline' onClick = {onLogout}>
                 Logout
             </button>
         </div>
